2f5b648cf36fba2c2a6a41b074fab56fe4eb925d
BSSE24058 <rafaykhan99999@gmail.com>
2025-08-27 15:55:54 +0500

    ..

--- CommentsAndVariables.py
+++ CommentsAndVariables.py
@@ -1,4 +1,4 @@
-print("Hello, my name is rafay its my first day leaning Python")
+print("\nHello, my name is rafay its my first day leaning Python\n")
 
 ## comments are some thing python ignores like
 # this is a comment
@@ -33,3 +33,30 @@ is_student = True
 # 1. camelCase (e.g. firstName)
 # 2. snake_case (e.g. first_name)
 # 3. PascalCase (e.g. FirstName)
+
+#################################################################################################################
+
+
+# Data Types
+# 1. int (integer)
+# 2. float (floating-point number)
+# 3. str (string)
+# 4. bool (boolean)
+# so basically what this means is that variables can hold different types of data in simple words what kind of data is being stored in the variable
+
+print("DATA TYPES")
+a = 5
+b = 3.14
+c = "Hello"
+d = True
+
+# There is a function name type() in Python that is used to determine the data type of a variable.
+print(type(a))  # Output: <class 'int'>
+print(type(b))  # Output: <class 'float'>
+print(type(c))  # Output: <class 'str'>
+print(type(d))  # Output: <class 'bool'>
+
+#There is another data type called complex (e.g. 1 + 2j)
+
+complex_num = 1 + 2j
+print(type(complex_num))  # Output: <class 'complex'>
