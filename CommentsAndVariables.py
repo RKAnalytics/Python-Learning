fd84c417a03228e819efcf4f8deb72e1c0974e9d
BSSE24058 <rafaykhan99999@gmail.com>
2025-08-27 15:08:34 +0500

    ..

---
+++ CommentsAndVariables.py
@@ -0,0 +1,35 @@
+print("Hello, my name is rafay its my first day leaning Python")
+
+## comments are some thing python ignores like
+# this is a comment
+#so comments are something that cant be executed by python
+
+# doc string """ it is a multiline comment when we  have to comment multiple line so we use multiline comments"""
+
+""" helloooo my name is dragon whats
+your name? """
+
+
+#################################################################################################################
+
+# variables are used to store information it can be any data type int, float, string, boolean 
+name = "rafay"
+age = 20
+is_student = True
+
+# 1name wrong (will give an error)
+# space not allowed before the variable name
+# dont use special characters like @, #, $, %, ^, &, *, (, )
+
+#################################################################################################################
+
+# naming conventions
+# 1. variable names should be descriptive
+# 2. use underscores to separate words (e.g. first_name)
+# 3. start variable names with a lowercase letter
+# 4. avoid using reserved keywords (e.g. if, else, for)
+# 5. keep variable names concise but meaningful
+# there are following types of writing variable names
+# 1. camelCase (e.g. firstName)
+# 2. snake_case (e.g. first_name)
+# 3. PascalCase (e.g. FirstName)
