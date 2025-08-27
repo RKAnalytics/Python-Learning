96357bf32a4b8b2aa56c9452be36da25b2ab63e8
BSSE24058 <rafaykhan99999@gmail.com>
2025-08-27 23:54:30 +0500

    ..

--- 3.InputsAndOutputs.py
+++ 3.InputsAndOutputs.py
@@ -8,4 +8,5 @@ print ("Hello, my name is", name, "and I am", age, "years old.") # this is calle
 # now the formatted string is where we use only one string and in which we can directly embed variables containing integer values
 print(f"Hello, my name is {name} and I am {age} years old.") # this is called f-string formatting
 # now the raw string is where we use a string as it is without any formatting
-print(r"Hello, my name is {name} and I am {age} years old.") # this is called raw string formatting
+print(r"C:\Users\Alice\Documents")# this is called raw string formatting
+# r-string treats backslashes as literal characters instead of escape characters
