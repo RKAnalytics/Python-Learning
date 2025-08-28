95df4b3daf90d5a65d56970755edaa0949923580
BSSE24058 <rafaykhan99999@gmail.com>
2025-08-28 18:28:47 +0500

    ..

--- 5.If_Else.py
+++ 5.If_Else.py
@@ -1,1 +1,43 @@
- 
+# If-Else Statements, so the concept of this is to allow the program to make decisions based on certain conditions.
+
+age = 18
+if age >= 18:
+    print("You are eligible to vote.")
+else:
+    print("You are not eligible to vote.")
+
+# We can also make Nested if-Else statements
+
+age = 29
+if age > 25:
+    if age == 30:
+        print("You are 30 years old.")
+        if age >= 31:
+            print("You are older than 30.")
+    else:
+        print("You are older than 25 but not 30.")
+else:
+    print("You are not older than 25.")
+
+# here first we checked the condition if age > 25:
+# if it is True then we checked the next condition if age == 30:
+# Since age is 29, this condition is False, so we go to the else block
+# In the else block, we print "You are older than 25 but not 30."
+
+
+## there is another thing called indentation, it is very important in python as it 
+# defines the blocks of code. In the above example, the code inside the if statements
+# is indented, which means it belongs to that particular if block. If we don't indent
+# the code properly, we might get unexpected results or errors.
+
+
+money = 15
+
+if money == 10: # if this condition is True it will print "I will buy a chocolate bar" if this condition is false then it will move to the next block
+    print ("I will buy a chocolate bar")
+
+elif money < 10: # if this condition is True it will print "I dont have enough money" if not met move towards the next block
+    print ("I dont have enough money")
+
+else: 
+    print ("I will buy a gum")
