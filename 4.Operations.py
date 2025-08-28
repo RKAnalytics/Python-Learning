07f8bd282bde08d1a12f473965a0bc7a3d1edeeb
BSSE24058 <rafaykhan99999@gmail.com>
2025-08-28 14:03:03 +0500

    ..

---
+++ 4.Operations.py
@@ -0,0 +1,22 @@
+# Operators
+# there are following operators
+
+# Arithimatic Operators 
+# 1. + (Addition)
+# 2. - (Subtraction)        
+# 3. * (Multiplication)
+
+# 4. / (Division) it always return float value
+# example: 5/2 = 2.5
+
+# 5. % (Modulus) it gives remainder
+# example: 5%2 = 1
+
+# 6. ** (Exponentiation) its like power example 2**3 = 2*2*2 = 8
+""" 
+But when we use this with strings it works like this
+example: "Hello" * 3 = HelloHelloHello
+
+"""
+# 7. // (Floor Division) always return integer value
+# example: 5//2 = 2
